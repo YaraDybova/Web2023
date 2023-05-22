de34673e96904e510968810d3d868d6fc2649f3e
Ярослава Дыбова <90902647+YaraDybova@users.noreply.github.com>
2023-05-22 20:15:01 +0300

    Update test_task_5_1b.py

--- 5/test_task_5_1b.py
+++ 5/test_task_5_1b.py
@@ -92,3 +92,9 @@ def test_task_sw1(capsys, monkeypatch):
     assert (
         correct_stdout in out.strip()
     ), "На стандартный поток вывода выводится неправильный вывод"
+    
+print("Введите имя устройства: ")
+name = input()
+print("Введите имя параметра (" + ', '.join(list(london_co[name])) + "): ")
+parameter = input()
+print(london_co[name][parameter])
